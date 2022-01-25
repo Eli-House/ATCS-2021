0c81f6f1793cd6e50294a07c2e4f41d33e86968d
Eli-House <ethousenbold@gmail.com>
2022-01-25 11:10:15 -0800

    Binary Tree

--- binaryTree/bst.py
+++ binaryTree/bst.py
@@ -15,7 +15,9 @@ Each node on the tree should be followed by a '-'.
 Ex. "1-2-3-4-5-"
 '''
 def getInorder(root):
-    return ''
+    if root == None:
+        return ''
+    return getInorder(root.left) + str(root.val) + "-" + getInorder(root.right)
 
 
 '''
@@ -26,7 +28,9 @@ Ex. "1-2-3-4-5-"
 '''
 # A function to do postorder tree traversal
 def getPostorder(root):
-    return ''
+    if root == None:
+        return ''
+    return getPostorder(root.left) + getPostorder(root.right) + str(root.val) + "-"
 
 
 '''
@@ -36,7 +40,9 @@ Each node on the tree should be followed by a '-'.
 Ex. "1-2-3-4-5-"
 '''
 def getPreorder(root):
-    return ''
+    if root == None:
+        return ''
+    return str(root.val) + "-" + getPreorder(root.left) + getPreorder(root.right)
 
 
 '''
@@ -47,7 +53,17 @@ original root with no change if the key already
 exists in the tree.
 '''
 def insert(root, key):
-    return root
+
+    if root == None:
+        return Node(key)
+
+    if root.val > key:
+        root.left = insert(root.left, key)
+        return root
+    elif root.val < key:
+        root.right = insert(root.right, key)
+        return root
+
 
 
 '''
@@ -55,7 +71,17 @@ Challenge: A function determines if a binary tree
 is a valid binary search tree
 '''
 def isBST(root):
-    return False
+    if root == None:
+        return True
+    if root.left != None:
+        if root.left.val >= root.val:
+            return False
+    if root.right != None:
+        if root.right.val <= root.val:
+            return False
+    return isBST(root.left) and isBST(root.right)
+
+
 
 
 if __name__ == '__main__':
@@ -78,3 +104,7 @@ if __name__ == '__main__':
     root = insert(root, 8)
     print("\nInorder traversal of binary tree with 8 inserted is")
     print(getInorder(root))
+
+    print("\nBinary")
+    print(isBST(root))
+
