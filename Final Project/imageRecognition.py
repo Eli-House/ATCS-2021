0ad67e8823801b0b70bc0bfb11eb2d1d0045c5f5
Eli-House <ethousenbold@gmail.com>
2022-05-10 09:25:21 -0700

    Eye and Face Detection

--- Final Project/imageRecognition.py
+++ Final Project/imageRecognition.py
@@ -2,18 +2,18 @@
 import cv2
 import numpy as np
 import glob
-import tensorflow as tf
-import pandas as pd
 
 # Load image
 def loadImage(filename):
     image = cv2.imread(filename)
     return image
 
+# Turn folder of photos into an array
 def loadFolder(path):
     folder = glob.glob(path)
     return folder
 
+# Alter image for better detection
 def changeImage(image):
     # Convert image into gray scale
     grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -21,6 +21,7 @@ def changeImage(image):
     grayImageBlurred = cv2.blur(grayImg, (5,5))
     return grayImageBlurred
 
+# Print out image with a title and duration
 def showImage(title, image, seconds):
     # print out image
     cv2.imshow(title, image)
@@ -51,7 +52,15 @@ if locatedCircles is not None:
 
 cPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cPath)
+eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# faceImg = loadImage("img/ACDC.jpeg")
+# grayFaceImg = changeImage(faceImg)
+# faces = faceCascade.detectMultiScale(grayFaceImg, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
+# Draw rectangles around faces
+#for (x, y, w, h) in faces:
+    #cv2.rectangle(faceImg, (x, y), (x+w, y+h), (167, 200, 45), 2)
+#showImage("Find Faces", faceImg, 0)
 video_capture = cv2.VideoCapture(0)
 
 while True:
@@ -59,20 +68,33 @@ while True:
     # faceImg = loadImage("img/ACDC.jpeg")
     grayFaceImg = changeImage(webCam)
     faces = faceCascade.detectMultiScale(grayFaceImg, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    eyes = eyeCascade.detectMultiScale(grayFaceImg, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     # Draw rectangles around detected faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(webCam, (x, y), (x+w, y+h), (167, 200, 45), 2)
-    #showImage("Faces", webCam, 0)
-    cv2.imshow('Video', webCam)
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(webCam, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
+    for (x, y, w, h) in faces:
+        cv2.rectangle(webCam, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        webCam = cv2.putText(webCam, 'No Mask', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        eyeGray = grayFaceImg[y:y+h, x:x+w]
+        eyeColor = webCam[y:y + h, x:x + w]
+        eyes = eyeCascade.detectMultiScale(eyeGray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        for (eyesX, eyesY, eyesW, eyesH) in eyes:
+            cv2.rectangle(eyeColor, (eyesX, eyesY), (eyesX+eyesW, eyesY+eyesH), (0, 0, 255), 2)
+
+    # Shows the webCam Photo. My method for showing images only works for pictures and not video
+    cv2.imshow('Mask?', webCam)
+
+    # This line runs the program until the q key is pressed. The q key will quit the program
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+#
 video_capture.release()
 cv2.destroyAllWindows()
+
 # Show image
 # showImage("Eye", img, 0)
-
 # Test for reading a folder
 #maskFolder = loadFolder("./withMask/*.png")[75]
 #singlePhoto = loadImage(maskFolder)
